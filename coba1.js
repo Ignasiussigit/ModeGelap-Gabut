6435cbcd166a24fbd6d1d374f29ab7d4b98b6fab
Ignasiussigit <martixdna@gmail.com>
2022-08-25 19:19:06 +0700

    ModeGelap

---
+++ coba1.js
@@ -0,0 +1,32 @@
+let toggleBtn = document.getElementById('toggle-btn');
+let body = document.body;
+let darkMode = localStorage.getItem('dark-mode');
+
+const enableDarkMode = () => {
+    toggleBtn.classList.replace('fa-sun', 'fa-moon');
+    body.classList.add('dark');
+    localStorage.setItem('dark-mode', 'enebled');
+}
+
+const disableDarkMode = () => {
+    toggleBtn.classList.replace('fa-moon', 'fa-sun');
+    body.classList.remove('dark');
+    localStorage.setItem('dark-mode', 'disabled');
+}
+
+if (darkMode === 'enabled') {
+    enableDarkMode();
+}
+
+toggleBtn.onclick = (e) => {
+    darkMode = localStorage.getItem('dark-mode');
+    if (darkMode === 'disabled') {
+        enableDarkMode();
+        document.getElementById('malam').style.display = 'block';
+        document.getElementById('pagi').style.display = 'none';
+    } else {
+        disableDarkMode();
+        document.getElementById('malam').style.display = 'none';
+        document.getElementById('pagi').style.display = 'block';
+    }
+}
